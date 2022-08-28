0aa0c16ca6b9a94106b8b7563cf9fe07d0e63f3a
Dr-Radhakrishna-Naik <naikradhakrishna@gmail.com>
2022-08-28 15:38:11 +0530

    Ensemble Learning assignments

---
+++ Ensemble-Techniques/Stacking_provided by Sampath.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 25 13:27:00 2022
+
+@author: Dell
+"""
+import pandas as pd
+data = pd.read_csv("C:/360DG/Datasets/Diabeted_Ensemble.csv")
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+xa = scaler.fit_transform(data.iloc[:, :-1])
+predictors = pd.DataFrame(xa)
+target = pd.get_dummies(data.iloc[:, -1], drop_first = True)
+# Train Test partition of the data
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(predictors, target, test_size = 0.2, random_state=0)
+# compare standalone models for binary classification
+from numpy import mean
+from numpy import std
+
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from matplotlib import pyplot
+
+X = x_train
+y = y_train
+# get a list of models to evaluate
+def get_models():
+	models = dict()
+	models['lr'] = LogisticRegression()
+	models['knn'] = KNeighborsClassifier()
+	models['cart'] = DecisionTreeClassifier()
+	models['svm'] = SVC()
+	models['bayes'] = GaussianNB()
+	return models
+# evaluate a given model using cross-validation
+def evaluate_model(model, X, y):
+	cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+	scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
+	return scores
+# get the models to evaluate
+models = get_models()
+# evaluate the models and store results
+results, names = list(), list()
+for name, model in models.items():
+	scores = evaluate_model(model, X, y)
+	results.append(scores)
+	names.append(name)
+	print('>%s %.3f (%.3f)' % (name, mean(scores), std(scores)))
+# plot model performance for comparison
+pyplot.boxplot(results, labels=names, showmeans=True)
+pyplot.show()
